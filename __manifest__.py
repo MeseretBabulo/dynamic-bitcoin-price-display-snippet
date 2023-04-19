4c81651ce63934b47c9a147279ae9176037800a4
MeseretBabulo <mesizobezo2014@gmail.com>
2023-04-19 10:57:51 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    "name": "Bitcoin price display Snippet",
+    "summary": "",
+    "category": "Website",
+    "version": "13.0.1.1.0",
+    "website": "",
+    "author": "Adgensee, Oondeo",
+    "license": "LGPL-3",
+    
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/snippets.xml",
+    ],
+
+    "application": False,
+    "installable": True,
+}
